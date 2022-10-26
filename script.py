959fe58eb38fed6e6446d950d10f98e9177b7e29
azzamallow <azzamallow@fastmail.com>
2022-10-27 09:00:27 +1100

    super awesome analysis done

---
+++ script.py
@@ -0,0 +1,39 @@
+import json
+
+protein_data = [
+  {
+    "conditionComparison": "S1 - S2",
+    "up.condition": "S1",
+    "down.condition": "S2",
+    "fdrLimit": 0.00336921246864423,
+    "data": [
+      {
+        "GroupId": 1,
+        "GroupLabel": "AAAA",
+        "GroupLabelType": "ProteinAccession",
+        "ProteinIds": "AAAA",
+        "GeneNames": "GNAAAA",
+        "Description": "Protein AA-RON",
+        "FoldChange": -0.0551770099399853,
+        "AdjustedPValue": 0.902516404445127,
+        "PValue": 0.7
+      },
+      {
+        "GroupId": 7,
+        "GroupLabel": "BBBB",
+        "GroupLabelType": "ProteinAccession",
+        "ProteinIds": "BBBB",
+        "GeneNames": "GNBBBB",
+        "Description": "Protein BB-BRON",
+        "FoldChange": 0.154339623642233,
+        "AdjustedPValue": 0.911776448109943,
+        "PValue": 0.4
+      }
+    ]
+  }
+]
+
+json_object = json.dumps(protein_data)
+ 
+with open("protein_viz.json", "w") as outfile:
+    outfile.write(json_object)
